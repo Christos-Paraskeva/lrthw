0cb4ecf805a0a2bd97fcdfda82a78abf6244f323
Christos <chris.paraskeva10@gmail.com>
2017-01-19 20:51:13 +0000

    completed chapter 28 & 29. I'm changing the order I complete these exercises

---
+++ chap28/ex1.rb
@@ -0,0 +1,14 @@
+# != not equal to
+# ! not
+# == equal to
+# < less than
+# > greater than
+# <= less than or equal to
+# >= more than or equal to
+# <=> combined comparision operator
+# += adds the right statement to the left
+# -= subtracts right statement from left
+# *= multiplies left statement by right
+# /= divides left statement by right
+# && both sides must evaluate to the same bootlean
+# || true if one of the sides is true
